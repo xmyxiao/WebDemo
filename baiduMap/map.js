1ad1b540bfed0b11ce66e38be454371a0cced53f
xmyxiao <958389530@qq.com>
2017-04-12 22:00:20 +0800

    百度地图demo

---
+++ baiduMap/map.js
@@ -0,0 +1,21 @@
+(function(global){
+	var companyMap;
+	companyMap = global.mapPage = {};
+	companyMap = {
+		map:null
+	}
+})(this);
+
+function mapPageInt(){
+	companyMap.map = new BMap.Map("companyMap");
+	companyMap.map.enableScrollWheelZoom();
+	//地图定位
+	var point = new BMap.Point(118.1951,24.532547);
+	companyMap.map.centerAndZoom(point, 12);
+	var infoWindow = new BMap.InfoWindow(document.getElementById("map-address0").innerHTML);
+	var marker = new BMap.Marker(point);
+	marker.addEventListener("click", function(){          
+	   this.openInfoWindow(infoWindow);
+	});
+	companyMap.map.addOverlay(marker);
+}
