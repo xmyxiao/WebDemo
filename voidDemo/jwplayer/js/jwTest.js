e277dd5a5f4d33010d5b1ffdf20ec6c571772efa
21448 <958389530@qq.com>
2017-07-11 16:18:36 +0800

    样例修改

--- voidDemo/jwplayer/js/jwTest.js
+++ voidDemo/jwplayer/js/jwTest.js
@@ -1,5 +1,5 @@
 $(function(){
-	jwplayer("myElement").setup({
+	/*jwplayer("myElement").setup({
 	    playlist: [{
 	        image: "img/player.jpg",
 	        sources: [{
@@ -8,14 +8,39 @@ $(function(){
 	            file: ""
 	        }]
 	    }],
-	    height: 360,
 	    primary: "flash",
-	    autostart: true,//自动播放
-	    controls:false,//不显示控件按钮
-	    width: 640
+	    autostart: false,//自动播放
+	    controls:true//显示控件按钮
+	});*/
+	jwplayer("myElement").setup({
+		playlist: [{
+	        "file": "rtmp://live.hkstv.hk.lxdns.com/live/hks"
+	    },{
+	        "file": "img/sintel.mp4"
+	    }],
+	    /*playlist: [{
+	        image: "img/player.jpg",
+	        sources: [{
+	            file: "rtmp://live.hkstv.hk.lxdns.com/live/hks"
+	        },{
+	            file: "img/sintel.mp4"
+	        }]
+	    }],*/
+	    autostart:false,
+	    overstretch:true,
+	    width: "100%",
+		ratio: "100%",
+		responsive: true
 	});
 	//获取播放时间
-	//jwplayer("myElement").getDuration();
+	//jwplayer("myElement").getPosition();
 	//跳转视频播放时间 
 	//jwplayer("myElement").seek(120);
+	/*
+	jwplayer(0).playlistItem(1); //播放下标为1的视频
+
+	jwplayer(0).playlistNext();//播放下一个视频
+
+	jwplayer(0).playlistPrev();//播放上一个视频
+	 * */
 });
