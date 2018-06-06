2b5cf9077a7d1f2876c53fa73e1534a34bfb3bdf
21448 <958389530@qq.com>
2018-06-06 16:31:18 +0800

    demo

--- vueproject/webpack.config.js
+++ vueproject/webpack.config.js
@@ -1,6 +1,7 @@
 //webpack 必须采用commonjs写法  这里的path是node的内置模块
-let path = require('path');//专门用于处理路径  传入一个相对路径  传会一个绝对路径
+let path = require('path');//专门用于处理路径  传入一个相对路径  传出一个绝对路径
 console.log(path.resolve('./path'));  //以当前路径解析一个绝对路径
+let htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry:'./src/main.js',  //打包的入口文件  webpack会自动查找相关依赖进行打包  多个使用对象
 	output:{
@@ -13,7 +14,13 @@ module.exports = {
     rules: [
       {test:/\.js$/,use:'babel-loader',exclude:'/node_modules'},
       //use 时从右往左写
-      {test:/\.css$/,user:['style-loader','css-loader']}
+      {test:/\.css$/,use:['style-loader','css-loader']}
     ]
-  }
+  },
+  plugins:[
+    new htmlWebpackPlugin({//自动插入到dist目录中
+      template:'./src/index.html'//使用模板
+      //filename:'file'  可以改变文件名
+    })
+  ]
 }
