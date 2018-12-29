d95fed19377ab019a87d9b82138830e9ce351199
21448 <958389530@qq.com>
2018-12-29 16:17:17 +0800

    map

--- baiduMap/map.js
+++ baiduMap/map.js
@@ -11,6 +11,7 @@ function mapPageInt(){
 	companyMap.map.enableScrollWheelZoom();
 	//地图定位
 	var point = new BMap.Point(118.1951,24.532547);
+	//119.275161, 26.029327
 	companyMap.map.centerAndZoom(point, 12);
 	var infoWindow = new BMap.InfoWindow(document.getElementById("map-address0").innerHTML);
 	var marker = new BMap.Marker(point);
