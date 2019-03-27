a1d9e3d5dc28bc24b42b3b1dae34a4f9d85ce91d
21448 <958389530@qq.com>
2019-03-27 20:04:28 +0800

    roll

---
+++ rollup-test/src/aaa.js
@@ -0,0 +1,15 @@
+console.log('a');
+//根据name获取cookie
+window.getCookie = function(name) {
+	var cookies = document.cookie;
+	var list = cookies.split("; ");
+
+	for(var i = 0; i < list.length; i++) {
+		var arr = list[i].split("=");
+		if(arr[0] == name){
+			return decodeURIComponent(arr[1]);
+		}
+	}
+  return "";
+}
+console.log(getCookie('user'));
